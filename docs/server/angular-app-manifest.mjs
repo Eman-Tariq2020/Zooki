3e7b7b3b051188a8a7195b2bf35982468875c49f
unknown <emannt501@gmail.com>
2025-11-10 20:38:32 +0500

    first commit

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -7,8 +7,8 @@ export default {
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 5521, hash: 'f525f2a55a7062a446547e3084b3b98a14ab6d9722e17ab0fcd6d6318da9e40b', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1163, hash: 'd517e49cb42287c0ea71e08cf1ba489ddb1fa0c72bd203b491b6e10bbcc8e6ff', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 5521, hash: 'dfd59575c7b0097e1181014eaf7b74abc72711e1bc173e0cfe2d278f74dfb362', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1163, hash: '3412c5408efa8891d7f951e858e1a5fd37cfcdaad04711a0b2c1fd8b96f90a33', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-WAAKCHGR.css': {size: 305079, hash: 'KgUj/5es9lQ', text: () => import('./assets-chunks/styles-WAAKCHGR_css.mjs').then(m => m.default)}
   },
 };
